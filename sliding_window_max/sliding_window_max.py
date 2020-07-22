d5ce10ebd0f968efb4701f28a873242b9d9928aa
cjogles <jackson.ogles28@gmail.com>
2020-07-22 14:48:44 -0600

    finished test sliding window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,14 +3,21 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
-
-    pass
+    window_sums = []
+    for i in range(len(nums)):
+        if i+k == len(nums)+1:
+            return window_sums
+        new_array = nums[i:i + k]
+        sum = new_array[0]
+        for i in new_array:
+            if i > sum:
+                sum = i
+        window_sums.append(sum)
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
-    arr = [1, 3, -1, -3, 5, 3, 6, 7]
+    arr = [1,3,-1,-3,5,3,6,7]
     k = 3
 
     print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
