17edab2041a1ff24a926bf5c6de214a8b657ff76
cjogles <jackson.ogles28@gmail.com>
2020-07-22 17:13:51 -0600

    working on last problem

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,28 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
 
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
+nterms = 10
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
+
+
+
+# if __name__ == "__main__":
+#     # Use the main function here to test out your implementation
+#     num_cookies = 6
+
+#     print(
+#         f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
